9c1a7bea41a847ffdd00f0e5b50c476d85d06093
ajaymodugu <ajaymodugu.int@gmail.com>
2025-08-21 17:38:06 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,15 @@
+CREATE DATABASE recipes_db;
+USE recipes_db;
 
+CREATE TABLE recipes (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    cuisine VARCHAR(100),
+    title VARCHAR(255),
+    rating FLOAT DEFAULT NULL,
+    prep_time INT DEFAULT NULL,
+    cook_time INT DEFAULT NULL,
+    total_time INT DEFAULT NULL,
+    description TEXT,
+    nutrients JSON,
+    serves VARCHAR(50)
+);
